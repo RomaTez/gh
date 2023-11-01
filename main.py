b0a7d7b7472fbe11b36b3faedfe64a2118e15afe
RomaTez <70908104+RomaTez@users.noreply.github.com>
2023-11-01 12:24:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def create_github_repository(repo_name):
         "name": repo_name,
         "auto_init": True
     }
-    response = requests.post('https://api.github.com/user/repos', json=json_data, headers=headers, auth=(username, password))
+    response = requests.post('https://api.github.com/user/repos', json=json_data, headers=headers, auth=("RomaTez", "04izaholSS!"))
     
     if response.status_code == 201:
         print(f"Репозиторий '{repo_name}' успешно создан на GitHub.")
