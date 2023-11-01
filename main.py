49e00edd29364800a7f50be8fc81d282582359aa
RomaTez <70908104+RomaTez@users.noreply.github.com>
2023-11-01 12:19:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-def create_github_repository(username, password, repo_name):
+def create_github_repository(repo_name):
     # Создание нового репозитория
-    headers = {'Authorization': 'token YOUR_ACCESS_TOKEN'}
+    headers = {'Authorization': 'token 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'}
     json_data = {
         "name": repo_name,
         "auto_init": True
@@ -12,3 +12,5 @@ def create_github_repository(username, password, repo_name):
         print(f"Репозиторий '{repo_name}' успешно создан на GitHub.")
     else:
         print("Не удалось создать репозиторий на GitHub.")
+name = input("Repository name")
+create_github_repository(name)
